52aa655e664da18646389c3ee5a254dea0c8be76
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-03-08 15:53:56 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+
+function Title(){
+  return <h1>I Am The Title !</h1>
+}
+
+function App() {
+  return (
+  <div>
+    <Title />
+    <button>Click Here</button>
+  </div>
+  )
+}
+
+export default App
