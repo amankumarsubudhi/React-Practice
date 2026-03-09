ab5351ec79417b5b29ebb462a9045c2f0d8561bd
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-03-09 23:37:14 +0530

    added a component

---
+++ src/Product.jsx
@@ -0,0 +1,13 @@
+import "./Product.css";
+
+function Product(){
+    return (
+        <div className="Product">
+            <h5>Product Title</h5>
+            <h6>Product Description</h6>
+        </div>
+    )
+}
+
+
+export default Product;
