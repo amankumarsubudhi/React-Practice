3136d89b2ed732995689d0f50efde6d0bdbe7f2a
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-03-10 00:57:49 +0530

    implements props

--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -4,9 +4,9 @@ import Product from "./Product";
 function ProductTab(){
     return (
         <>
-        <Product />
-        <Product />
-        <Product />
+        <Product title = "Smart Phones" price = "25000" />
+        <Product title = "Laptops" price = {65000} />
+        <Product title = "Desktop Monitors" price = "25000" />
         </>
     )
 }
