d789355de2cbca8ee9e49d706758206fcc0e73dd
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-03-10 11:59:59 +0530

    added an array

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,10 +1,12 @@
 import "./Product.css";
 
-function Product({title, price}){
+function Product({title, price, features}){
+    console.log(features);
     return (
         <div className="Product">
             <h4>{title}</h4>
             <h5>Price : {price}</h5>
+            <h6>{features}</h6>
         </div>
     )
 }
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -2,9 +2,10 @@ import Product from "./Product";
 
 
 function ProductTab(){
+    let options = ["200MP Camera","Sony Seansor","8500mah Battery"];
     return (
         <>
-        <Product title = "Smart Phones" price = "25000" />
+        <Product title = "Smart Phones" price = "25000" features = {options} />
         <Product title = "Laptops" price = {65000} />
         <Product title = "Desktop Monitors" price = "25000" />
         </>
