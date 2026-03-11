bdd1a85e07f73f17a9f64319a4151fc543d24470
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-03-11 16:39:18 +0530

    used conditional statements for conditional styling

--- src/Product.jsx
+++ src/Product.jsx
@@ -6,9 +6,14 @@ function Product({title, price, features1 = [], features2 = [], features3 = []})
     const list2 = features2.map((feature2)=> <li>{feature2}</li>);
     const list3 = features3.map((feature3 => <li>{feature3}</li>));
 
+    let havingDiscount = price > 85000;
+
+    let styles = {backgroundColor: price>40000 || havingDiscount? "darkred" : "darkgreen"};
+    
+
     
     return (
-        <div className="Product">
+        <div className="Product" style={styles}>
             <h4>{title}</h4>
             <h5>Price : {price}</h5>
             {price > 40000 ? <p>25% Discount</p> : <p>10% Discount</p>}
