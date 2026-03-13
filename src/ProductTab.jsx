04cbdf7e149f5bfafa9e9aba494c103ba863e512
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-03-13 22:04:38 +0530

    modified in components

--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,14 +1,15 @@
 import Product from "./Product";
+import "./ProductTab.css";
 
 
 function ProductTab(){
     let options1 = ["200MP Camera","Sony Seansor","8500mah Battery"];
     return (
-        <>
+        <div className="ProductTab">
         <Product title = "Smart Phones" price = "25000" features1 = {options1} />
         <Product title = "Laptops" price = {65000} features2={["AMD Ryzen 7 8000 Series", "32 Cores, 64 Threads", "NVIDIA RTX 3060 16 GB"]} />
         <Product title = "Desktop Monitors" price = "25000" features3={["4K Resolution", "IPS Display", "99% sRGB"]} />
-        </>
+        </div>
     )
 }
 
