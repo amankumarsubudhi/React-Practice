cf9c24f4152c4384e705fda338cb04192ef98999
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-03-15 23:09:41 +0530

    added a new component

---
+++ src/ButtonCard.jsx
@@ -0,0 +1,21 @@
+function ButtonCard() {
+  return (
+    <div className="buttons">
+      <div>
+        <button>
+          <a href="https://amankumarsubudhi.github.io">Portfolio</a>
+        </button>
+      </div>
+
+      <div>
+        <button>
+          <a href="https://github.com/amankumarsubudhi" target="_blank">
+            Follow Me On Github
+          </a>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ButtonCard;
