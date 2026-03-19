041d068025cc32c26c2ac5b314ef2495d8a198a5
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-03-19 23:06:38 +0530

    updated message

--- src/MesBox.jsx
+++ src/MesBox.jsx
@@ -5,7 +5,7 @@ function MesBox({userName, textColor}){
     return(
         <>
         <h1 style={style}>Hi, {userName}</h1>
-        <h3>Welcome To My Dashboard</h3>
+        <h3>Welcome To The Dashboard</h3>
         </>
     );
 }
