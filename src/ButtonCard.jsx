a99077060ba53fe64f3de0a3821bbfcc12166fb9
Aman Kumar Subudhi <aman.work404@gmail.com>
2026-03-22 23:48:02 +0530

    updated button

--- src/ButtonCard.jsx
+++ src/ButtonCard.jsx
@@ -12,7 +12,7 @@ function ButtonCard() {
       <div>
         <button>
           <a href="https://github.com/amankumarsubudhi" target="_blank">
-          Follow Github
+          Follow Me On Github
           </a>
         </button>
       </div>
